f842c4c2c12fc571c6b8da764bfe1fb8a64e4fb6
Rayan84 <rayan_84@live.com>
2022-06-01 14:34:13 +0200

    Add API fetching functionality

--- app/javascript/components/Editor.js
+++ app/javascript/components/Editor.js
@@ -1,1 +1,35 @@
 import React, { useState, useEffect } from "react";
+import Header from './Header';
+import EventList from './EventList';
+
+const Editor = () => {
+  const [events, setEvents] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+    try {
+      const response = await window.fetch('/api/events');
+      if (!response.ok) throw Error(response.statusText);
+      const data = await response.json();
+      setEvents(data);
+    } catch (error) {
+      setIsError(true);
+      console.error(error);
+    }
+    setIsLoading(false);
+    };
+  fetchData();
+  }, [])
+  return (
+    <>
+      <Header />
+      {isError && <p>Something went wrong. check the console.</p>}
+
+      {isLoading ? <p>Loading..</p> : <EventList events={events} />}
+    </>
+  );
+};
+
+export default Editor;
